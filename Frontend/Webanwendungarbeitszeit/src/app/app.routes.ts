12f6b5ab288a7f0a5ff5df91c7704b4d46867335
Luca S <Luca.Scheidhauer@edu.siemens-energy.com>
2024-11-19 14:41:53 +0100

    Add Routing

--- Frontend/Webanwendungarbeitszeit/src/app/app.routes.ts
+++ Frontend/Webanwendungarbeitszeit/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import {AnzeigeComponent} from "./anzeige/anzeige.component";
+import {EingabeComponent} from "./eingabe/eingabe.component";
 
-export const routes: Routes = [];
+export const MyRoutes: Routes = [
+    {path: 'anzeige', component: AnzeigeComponent},
+    {path: 'eingabe', component: EingabeComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(MyRoutes)],
+    exports: [RouterModule]
+  })
+
+  export class AppRoutingModule { }
