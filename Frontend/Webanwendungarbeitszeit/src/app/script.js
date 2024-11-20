2761caaa0d7721134e31707581a8e525736ee838
Luca S <Luca.Scheidhauer@edu.siemens-energy.com>
2024-11-20 13:06:16 +0100

    edit button event Handler

--- Frontend/Webanwendungarbeitszeit/src/app/script.js
+++ Frontend/Webanwendungarbeitszeit/src/app/script.js
@@ -1,23 +1,7 @@
 document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Verhindert das Standardverhalten des Formulars
 
-    // Hier könntest du eine Validierung oder Authentifizierung durchführen
-    const email = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    it('Get html', function(done) {
-        request('localhost', function(err, res, body) {
-          assert.equal(body, 'html');
-          done();
-        });
-      });
-
-    // Beispielvalidierung (einfacher Check)
-    if (email && password) {
-        // Login erfolgreich
         document.getElementById('login-container').classList.add('hidden');
         document.getElementById('content').classList.remove('hidden');
-    } else {
-        alert('Bitte Username und Passwort eingeben.');
     }
-});
+);
