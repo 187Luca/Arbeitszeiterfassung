4f266d5d8e23f78c2fc0c3bf33a914fb42f29378
Luca S <Luca.Scheidhauer@edu.siemens-energy.com>
2024-11-20 08:28:39 +0100

    Change Website concept

--- Frontend/Webanwendungarbeitszeit/src/app/app.routes.ts
+++ Frontend/Webanwendungarbeitszeit/src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import {AnzeigeComponent} from "./anzeige/anzeige.component";
-import {EingabeComponent} from "./eingabe/eingabe.component";
 
 export const MyRoutes: Routes = [
-    {path: 'anzeige', component: AnzeigeComponent},
-    {path: 'eingabe', component: EingabeComponent}
+    {path: 'anzeige', component: AnzeigeComponent}
 ];
 
 @NgModule({
