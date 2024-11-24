1f5f733eb39c200ef103b3a5a83dd2eda2d46c40
Luca S. <Luca.Scheidhauer@edu.siemens-energy.com>
2024-11-24 11:53:18 +0100

    Add eingabe routing

--- Frontend/Webanwendungarbeitszeit/src/app/app.routes.ts
+++ Frontend/Webanwendungarbeitszeit/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import {AnzeigeComponent} from "./anzeige/anzeige.component";
+import { EingabeComponent } from './eingabe/eingabe.component';
 
 export const MyRoutes: Routes = [
-    {path: 'anzeige', component: AnzeigeComponent}
+    {path: 'anzeige', component: AnzeigeComponent},
+    {path: 'eingabe', component: EingabeComponent}
 ];
 
 @NgModule({
