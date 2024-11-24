dc735df486b210acbdeac46a3e6d45ca5670cc78
Lukas Whmn <Lukas.Wehrmann@edu.siemens-energy.com>
2024-11-24 12:49:50 +0100

    Kleinigkeit

--- Backend/node.js
+++ Backend/node.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
-const express = require('express');
+//const path = require('path');
+//const express = require('express');
 
 
 //Login Datenbankabgleich
